f891337d37e04c4a28d3fb4a151f7fc803388d63
jqjiang <charmeryl@gmail.com>
2017-01-26 00:01:03 +0800

    demo: update params

--- app/src/main/java/com/charmeryl/appupdaterdemo/MainActivity.java
+++ app/src/main/java/com/charmeryl/appupdaterdemo/MainActivity.java
@@ -1,5 +1,8 @@
 package com.charmeryl.appupdaterdemo;
 
+import android.content.SharedPreferences;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
@@ -19,13 +22,28 @@ public class MainActivity extends AppCompatActivity {
 
     private void initView(){
         Button btn_update_avail = (Button) findViewById(R.id.btn_update_avail);
+        Button btn_update_avail_auto = (Button) findViewById(R.id.btn_update_avail_auto);
         Button btn_update_none = (Button) findViewById(R.id.btn_update_none);
+
+        final FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
 
         btn_update_avail.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 new AppUpdater(MainActivity.this)
                         .setTriggerMethod(AppUpdater.TriggerMethod.MANUALLY)
+                        .setSnackbarView(fab)
+                        .setUpdateUrl("https://gist.githubusercontent.com/CharmeRyl/efa87949bd3724221b02ea4d01919ba9/raw/51557b836999e298d445e695438e42f1cfe3b0e5/updater.json")
+                        .start();
+            }
+        });
+
+        btn_update_avail_auto.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                new AppUpdater(MainActivity.this)
+                        .setTriggerMethod(AppUpdater.TriggerMethod.AUTOMATIC)
+                        .setSnackbarView(fab)
                         .setUpdateUrl("https://gist.githubusercontent.com/CharmeRyl/efa87949bd3724221b02ea4d01919ba9/raw/51557b836999e298d445e695438e42f1cfe3b0e5/updater.json")
                         .start();
             }
@@ -36,8 +54,18 @@ public class MainActivity extends AppCompatActivity {
             public void onClick(View v) {
                 new AppUpdater(MainActivity.this)
                         .setTriggerMethod(AppUpdater.TriggerMethod.MANUALLY)
+                        .setSnackbarView(fab)
                         .setUpdateUrl("https://raw.githubusercontent.com/charmeryl/AppUpdater/master/app/update-latest.json")
                         .start();
+            }
+        });
+
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                SharedPreferences.Editor editor = getSharedPreferences("updater",0).edit();
+                editor.clear().apply();
+                Snackbar.make(v,"Pref cleared",Snackbar.LENGTH_SHORT).show();
             }
         });
     }
